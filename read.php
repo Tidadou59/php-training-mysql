8aa075e442eea5d7f140f1e8857d6a4a15b7470b
David Pronnier <43751799+Tidadou59@users.noreply.github.com>
2019-01-17 14:36:40 +0100

    Add files via upload

--- read.php
+++ read.php
@@ -1,9 +1,34 @@
 <?php
 include ("connexion.php"); //connexion bdd
+// démarre la session
+//session_start (); (déjà rajouté dans le fichier "connexion.php")
 /* ***************************************************************** */
 
 $sql = "select * from hiking";
 $resultat = $conn->query($sql);
+
+//session_unset();
+//session_destroy();
+
+// On récupère les variables de session
+if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
+
+    // test pour voir si nos variables ont bien été enregistrées
+    echo '<html>';
+    echo '<head>';
+    echo '<title>Page de notre section membre</title>';
+    echo '</head>';
+
+    echo '<body>';
+    echo 'Bonjour '.$_SESSION['username'].', Vous êtes connecté à votre session.';
+    echo '<br />';
+
+    // On affiche un lien pour fermer notre session
+    echo '<a href="./logout.php">Déconnection</a>';
+}
+else {
+    echo 'Vous n\'êtes pas connecté à votre session...';
+}
 
 ?>
 
@@ -15,20 +40,32 @@ $resultat = $conn->query($sql);
     <link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
+  <menu> <a href="login.php">
+          <img class="imgModif" src="connex.jpg" height="50" width="50" >
+      </a> </menu>
+
+
+
     <h1>Liste des randonnées</h1>
     <center>
     <table class="table">
       <!-- Afficher la liste des randonnées -->
 
         <tr class="styl">
-            <th class="taille_Prenom-Nom"> Name </th>
-            <th class="taille_Prenom-Nom"> Difficulté </th>
-            <th class="taille_Prenom-Nom"> Distance </th>
-            <th class="taille_Prenom-Nom"> Durée </th>
-            <th class="taille_Prenom-Nom"> Dénivelé </th>
-
-            <th class="taille_Age"> Modifier </th>
-            <th class="taille_Age"> Supprimer </th>
+                        <th class="taille_Prenom-Nom"> Name </th>
+                        <th class="taille_Prenom-Nom"> Difficulté </th>
+                        <th class="taille_Prenom-Nom"> Distance </th>
+                        <th class="taille_Prenom-Nom"> Durée </th>
+                        <th class="taille_Prenom-Nom"> Dénivelé </th>
+            <?php
+                if(isset($_SESSION['username']))
+                    {
+            ?>
+                        <th class="taille_Age"> Modifier </th>
+                        <th class="taille_Age"> Supprimer </th>
+            <?php
+                    }
+            ?>
         </tr>
 
         <?php
@@ -42,30 +79,43 @@ $resultat = $conn->query($sql);
                 <td class='T_A'><?= $row['duration'] ?></td> <!-- Dénivelé -->
                 <td class='T_A'><?= $row['height_difference'] ?></td> <!-- Dénivelé -->
 
-                <td class='T_A'>
-                    <a href=" <?= 'update.php?id='.$row['id'] ?> ">
-                        <img class="imgModif" src="ico-modifier.jpg" height="50" width="50" >
-                    </a>
-                </td> <!-- Modif -->
+                <?php
+                    if(isset($_SESSION['username']))
+                        {
+                ?>
+
+                            <td class='T_A'>
+                                <a href=" <?= 'update.php?id='.$row['id'] ?> ">
+                                    <img class="imgModif" src="ico-modifier.jpg" height="50" width="50" >
+                            </a>
+                            </td> <!-- Modif -->
                 
-                <td class='T_A'> 
-                    <a href=" <?= 'delete.php?id='.$row['id'] ?> ">
-                        <img class="imgModif" src="ico-suppr.jpg" height="50" width="50" >
-                    </a>
-                </td> <!-- Modif -->
+                            <td class='T_A'>
+                                <a href=" <?= 'delete.php?id='.$row['id'] ?> ">
+                                    <img class="imgModif" src="ico-suppr.jpg" height="50" width="50" >
+                                </a>
+                            </td> <!-- Modif -->
+                <?php
+                        }
+                ?>
+
             </tr>
        <?php } ?>
-
-
 
     </table>
     </center>
 <br>
-    Ajouter une nouvelle donnée :<br>
-    <a href="create.php">
-            <img src="plus.png" height="30" width="30" >
-    </a>
-
+  <?php
+        if(isset($_SESSION['username']))
+            {
+  ?>
+                Ajouter une nouvelle donnée :<br>
+                <a href="create.php">
+                    <img src="plus.png" height="30" width="30" >
+                </a>
+  <?php
+            }
+  ?>
 
   </body>
 </html>
